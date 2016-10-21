432a5516a0b4aa7ed847e223a6986bdda492d7d7
Austin Walker <bgbrwr@gmail.com>
2016-10-21 16:04:07 -0600

    gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var order = require('gulp-order');
+
+gulp.task('js', function(){
+  return gulp.src(['./public/app/*.js', './public/app/js/*.js', './public/app/js/**/*.js'])
+  .pipe(plumber())
+  .pipe(order([
+    'app.js', '*.js'
+  ]))
+  .pipe(concat('all.js'))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task("sass", function(){
+  return gulp.src(["./public/styles/*.css", "./public/styles/*.scss"])
+  .pipe(sass().on('error', sass.logError))
+  .pipe(concat('all.css'))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./public/styles/*.css', ['sass']);
+  gulp.watch('./public/styles/*.scss', ['sass']);
+  gulp.watch('./public/app/*.js', ['js']);
+  gulp.watch('./public/app/js/*.js', ['js']);
+  gulp.watch('./public/app/js/**/*.js', ['js']);
+
+});
+
+gulp.task('default', ['watch', 'js', 'sass']);
