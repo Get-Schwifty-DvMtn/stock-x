87958c279e06a6c56064957815dd3bdab8b63386
Austin Walker <bgbrwr@gmail.com>
2016-10-24 15:20:36 -0600

    got an advaced prototype working

--- server/controller/yahooCtrl.js
+++ server/controller/yahooCtrl.js
@@ -1,15 +1,34 @@
 var server = require('../server.js');
+var yahooFinance = require('yahoo-finance');
+var today = new Date();
+var year = today.getFullYear();
+var month = today.getMonth();
+var date = today.getDate();
 
-var today = String(new date(year) + "-" + String(new date(month) + 1) + "-" + new date(day));
-//test -
-// yahooFinance.historical({
-//   symbol: 'AAPL',
-//   from: '2016-01-01',
-//   to: today,
-//   // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
-// }, function (err, quotes) {
-//   //...
-//   console.log(quotes);
-// });
+var todayFormatted = year + "-" + (month + 1) + "-" + date;
+//test
+var SYMBOLS = [
+  'AAPL',
+  'AMZN',
+  'GOOGL',
+  'YHOO'
+];
 
-angular.module("stock").controller()
+
+module.exports = {
+  getStocks: function(req, res) {
+    yahooFinance.historical({
+      symbols: SYMBOLS,
+      from: '2014-10-17',
+      to: todayFormatted,
+      // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
+    }, function (err, quotes) {
+      if (err){
+        res.status(400).json(err);
+      }
+      else {
+        res.status(200).json(quotes);
+      }
+    });
+  }
+};
