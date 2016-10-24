34bee6f7aa9e56f6f76a220e62412deb7a19afdc
Austin Walker <bgbrwr@gmail.com>
2016-10-24 11:07:46 -0600

    working on fixing gulp css

--- server/controller/yahooCtrl.js
+++ server/controller/yahooCtrl.js
@@ -1,12 +1,15 @@
 var server = require('../server.js');
 
+var today = String(new date(year) + "-" + String(new date(month) + 1) + "-" + new date(day));
 //test -
-yahooFinance.historical({
-  symbol: 'AAPL',
-  from: '2012-01-01',
-  to: '2012-01-04',
-  // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
-}, function (err, quotes) {
-  //...
-  console.log(quotes);
-});
+// yahooFinance.historical({
+//   symbol: 'AAPL',
+//   from: '2016-01-01',
+//   to: today,
+//   // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
+// }, function (err, quotes) {
+//   //...
+//   console.log(quotes);
+// });
+
+angular.module("stock").controller()
