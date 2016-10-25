1133cc0e60405648461c8c15a4f306be0aabc5cc
Austin Walker <Austin Walker>
2016-10-25 16:10:17 -0600

    Setting up Oauth

--- public/dist/all.js
+++ public/dist/all.js
@@ -1,10 +1,27 @@
 angular.module("stock", ["ui.router"])
   .config(function($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise("/");
-
+    
     $stateProvider
     .state("home", {
-      url: "/"
+      url: "/",
+      templateUrl: "./views/home.html"
+    })
+    .state("createUser", {
+      url: "/user/new",
+      templateUrl: "./views/createUser.html"
+    })
+    .state("profile", {
+      url: "/user/:id",
+      templateUrl: "./views/profile.html"
+    })
+    .state("profileStock", {
+      url: "/user/:id/:stock",
+      templateUrl: "./views/profileStock.html"
+    })
+    .state("pref", {
+      url: "/user/:id/pref",
+      templateUrl: "./views/pref.html"
     });
     //the rest of our states will go here
   });
