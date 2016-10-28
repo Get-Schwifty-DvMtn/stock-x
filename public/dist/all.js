ec76f2c6821db057513b4a3ef9147b1c72d5caed
goldboat90 <tyler.rymer@gmail.com>
2016-10-28 12:07:52 -0600

    Fixed bugs and styling issues with navbar

--- public/dist/all.js
+++ public/dist/all.js
@@ -35,6 +35,40 @@ angular.module("stock", ["ui.router"])
     });
   });
 
+angular.module('stock').component('headerComponent', {
+  templateUrl: "./js/templates/headerComponent.html"
+
+  });
+
+  angular.module('stock').directive('animation', function() {
+     return {
+         restrict: "EA",
+         link: function(scope, elem, attr) {
+            //  $(".navicon").click(function() {
+            //
+            //  });
+            //  $('body').on('click', '.navicon', function() {
+            //     $(this).toggleClass("active");
+            //     $(".wrapper").toggleClass("active");
+            //     $(".wrapper-overlay").toggleClass("active");
+            //     $("body").toggleClass("no-scroll");
+            // });
+            // $('body').on('click', '.navicon', function() {
+            //     $(".wrapper").click(function() {
+            //      $(this).removeClass("active");
+            //      $(".navicon").removeClass("active");
+            //      $(".wrapper-overlay").removeClass("active");
+            //      $("body").removeClass("no-scroll");
+            //  });
+         }
+     }
+  });
+
+angular.module('stock').component('navComponent', {
+  templateUrl: "./js/templates/navComponent.html"
+
+  });
+
 angular.module("stock")
   .component("nyTimesComponent", {
     templateUrl: "./js/templates/nyTimesComponent.html",
--- public/styles/css/jquery.js
+++ public/styles/css/jquery.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
 
-	$(".navicon").click(function() {
-		$(this).toggleClass("active");
+	$('body').on('click', '.navicon', function() {
+		$('.navicon').toggleClass("active");
+		$('.header').toggleClass("active");
 		$(".wrapper").toggleClass("active");
 		$(".wrapper-overlay").toggleClass("active");
 		$("body").toggleClass("no-scroll");
 	});
-	$(".wrapper").click(function() {
+
+	$('.wrapper').on('click', function() {
+		console.log('fired')
 		$(this).removeClass("active");
 		$(".navicon").removeClass("active");
+		$('.header').removeClass("active");
 		$(".wrapper-overlay").removeClass("active");
 		$("body").removeClass("no-scroll");
 	});
+	// $(".wrapper").click(function() {
+	// 	console.log('fired')
+	// 	$(this).removeClass("active");
+	// 	$(".navicon").removeClass("active");
+	// 	$(".wrapper-overlay").removeClass("active");
+	// 	$("body").removeClass("no-scroll");
+	// });
+
 
 	$(function(){
 	  $(".element").typed({
