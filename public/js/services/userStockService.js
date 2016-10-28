9238263915566500dab7d6c9b0ef2d9fed9e771d
Brad Frost <you@example.com>
2016-10-27 22:15:07 -0600

    worked with austin to pull user data from db and display in starredStocksComponent. We will eventually move it to the nav component.

--- public/js/services/userStockService.js
+++ public/js/services/userStockService.js
@@ -1,11 +1,13 @@
 angular.module("stock")
-  .service("selectStocksService", function($http){
+  .service("userStocksService", function($http){
     this.getAllStocks = function(){
       return $http.get("/getallstocks");
     };
     this.getSavedStocks = function(id){
       return $http.get("/user/" + id + "/getsavedstocks");
     };
-
+    this.getUserInfo = function(id){
+      return $http.get("getuserinfo/"+id);
+    };
 
 });
