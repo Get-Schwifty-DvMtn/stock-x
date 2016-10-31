b5f7b85271e2613766be47bfd117121cf4a4d576
dougglez <maxfield.douglas@gmail.com>
2016-10-31 16:30:51 -0600

    users can now add stocks to their watchlist

--- public/js/components/selectStocksComponent.js
+++ public/js/components/selectStocksComponent.js
@@ -1,14 +1,25 @@
 angular.module('stock').component('selectStocksComponent', {
   templateUrl: "./js/templates/selectStocksComponent.html",
-  controller: function selectStocksController(userStocksService, $scope){
-
+  controller: function selectStocksController(userStocksService, $scope, $stateParams){
+console.log("these are the state Params: ", $stateParams);
     userStocksService.getAllStocks().then(function(res){
       console.log("select", res.data);
       $scope.all_stocks = res.data;
+    });  //closes userStocksService function
+
+    $scope.addNewFavorite = function(stock) {
+
+      userStocksService.addNewFavorite([$stateParams.id, stock])
+        .then(function(res) {
+          console.log("newStock - res",res);
+          console.log("new stock symbol is: ", stock);
+
+      });
+
+    };
 
 
 
-    }); //closes userStocksService function
 
   }, //closes controller
   bindings: []
