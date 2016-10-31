c8fa0e2d4aab088d435b7c974751b35d886c3d2a
dougglez <maxfield.douglas@gmail.com>
2016-10-31 11:24:03 -0600

    committing mini changes to pull most recent merge

--- public/dist/all.js
+++ public/dist/all.js
@@ -100,6 +100,14 @@ angular.module('stock').component('selectStocksComponent', {
 
 });
 
+angular.module('stock').component('settingsComponent', {
+  templateUrl: "./js/templates/selectStocksComponent.html",
+  controller: function selectStocksController(userStocksService, $scope){
+
+  }, //closes controller
+  bindings: []
+
+});
 
 angular.module('stock').component('starredStocksComponent', {
   templateUrl: "./js/templates/starredStocksComponent.html",
---
+++ public/js/components/settingsComponent.js
@@ -0,0 +1,8 @@
+angular.module('stock').component('settingsComponent', {
+  templateUrl: "./js/templates/selectStocksComponent.html",
+  controller: function selectStocksController(userStocksService, $scope){
+
+  }, //closes controller
+  bindings: []
+
+});
