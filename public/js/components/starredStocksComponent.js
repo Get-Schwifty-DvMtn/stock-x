0cd2eeae87ff591f478910bd4b756c51439bbb03
Austin Walker <Austin Walker>
2016-10-31 11:10:03 -0600

    loads user info into the header

--- public/js/components/starredStocksComponent.js
+++ public/js/components/starredStocksComponent.js
@@ -1,40 +1,36 @@
 angular.module('stock').component('starredStocksComponent', {
-  templateUrl: "./js/templates/starredStocksComponent.html",
-  controller: function starredStocksController(userStocksService, yahooService, $scope, $stateParams){
+    templateUrl: "./js/templates/starredStocksComponent.html",
+    controller: function starredStocksController(userStocksService, yahooService, $scope, $stateParams) {
 
-    userStocksService.getSavedStocks($stateParams.id)
-      .then(function(res){
-        // console.log("starred", res.data);
+        userStocksService.getSavedStocks($stateParams.id).then(function(res) {
+            // console.log("starred", res.data);
 
-
-        //getting customers saved stocks for yahoo snaphsot
-        var savedStockSymbols = {
-              symbols: []
+            //getting customers saved stocks for yahoo snaphsot
+            var savedStockSymbols = {
+                symbols: []
             };
-        //passing saved stocks into a new array
-        for (var i = 0; i < res.data.length; i++) {
-          savedStockSymbols.symbols.push(res.data[i].company_symbol);
-        }
-        // console.log(savedStockSymbols);
-        //sending new array to backend for an api call
-        yahooService.getSnapshots(savedStockSymbols)
-        .then(function(res){
-          console.log(res.data);
-          $scope.saved_stocks = res.data;
-        }, function(err) {
-            console.log(err);
+            //passing saved stocks into a new array
+            for (var i = 0; i < res.data.length; i++) {
+                savedStockSymbols.symbols.push(res.data[i].company_symbol);
+            }
+            // console.log(savedStockSymbols);
+            //sending new array to backend for an api call
+            yahooService.getSnapshots(savedStockSymbols).then(function(res) {
+                $scope.saved_stocks = res.data;
+            }, function(err) {
+                console.log(err);
+            });
+
+            // console.log(res.data);
+        }); //closes selectStocksService function
+        userStocksService.getUserInfo($stateParams.id).then(function(res) {
+            // console.log(res);
+            $scope.firstName = res.data.firstName;
+            $scope.lastName = res.data.lastName;
+            $scope.userPic = res.data.pic;
         });
 
-        // console.log(res.data);
-    }); //closes selectStocksService function
-  userStocksService.getUserInfo($stateParams.id).then(function(res){
-    // console.log(res);
-    $scope.firstName= res.data.firstName;
-    $scope.lastName= res.data.lastName;
-    $scope.userPic= res.data.pic;
-  });
-
-  }, //closes controller
-  bindings: []
+    }, //closes controller
+    bindings: []
 
 });
