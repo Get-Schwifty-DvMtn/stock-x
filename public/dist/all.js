e56e7dd3753a36cd1f925c18fee3f62f43a57bf5
dougglez <maxfield.douglas@gmail.com>
2016-10-31 10:51:27 -0600

    committing all.js

--- public/dist/all.js
+++ public/dist/all.js
@@ -36,23 +36,32 @@ angular.module("stock", ["ui.router"])
   });
 
 angular.module('stock').component('headerComponent', {
-  templateUrl: "./js/templates/headerComponent.html",
-  controller: function headerController(userStocksService, $scope, $stateParams, $state){
+  templateUrl: "./js/templates/headerComponent.html"
 
+  });
 
-    //   var getUserInfo = function(){ userStocksService.getUserInfo($stateParams.id).then(function(res){
-    //     console.log(res);
-    //     // $scope.firstName= res.data.firstName;
-    //     // $scope.lastName= res.data.lastName;
-    //     // $scope.userPic= res.data.pic;
-    //   });
-    // };
-    // if ($stateParams.id){
-    //   console.log("yessir!");
-    //   getUserInfo();
-    // }
-  }
-
+  angular.module('stock').directive('animation', function() {
+     return {
+         restrict: "EA",
+         link: function(scope, elem, attr) {
+            //  $(".navicon").click(function() {
+            //
+            //  });
+            //  $('body').on('click', '.navicon', function() {
+            //     $(this).toggleClass("active");
+            //     $(".wrapper").toggleClass("active");
+            //     $(".wrapper-overlay").toggleClass("active");
+            //     $("body").toggleClass("no-scroll");
+            // });
+            // $('body').on('click', '.navicon', function() {
+            //     $(".wrapper").click(function() {
+            //      $(this).removeClass("active");
+            //      $(".navicon").removeClass("active");
+            //      $(".wrapper-overlay").removeClass("active");
+            //      $("body").removeClass("no-scroll");
+            //  });
+         }
+     }
   });
 
 angular.module('stock').component('navComponent', {
@@ -111,21 +120,22 @@ angular.module('stock').component('starredStocksComponent', {
         }
         // console.log(savedStockSymbols);
         //sending new array to backend for an api call
-        // yahooService.getSnapshots(savedStockSymbols)
-        // .then(function(res){
-        //   console.log(res.data);
-        //   $scope.saved_stocks = res.data;
-        // }, function(err) {
-        //     console.log(err);
-        // });
+        yahooService.getSnapshots(savedStockSymbols)
+        .then(function(res){
+          console.log(res.data);
+          $scope.saved_stocks = res.data;
+        }, function(err) {
+            console.log(err);
+        });
 
         // console.log(res.data);
     }); //closes selectStocksService function
-  // userStocksService.getUserInfo($stateParams.id).then(function(res){
-  //   $scope.firstName= res.data.firstName;
-  //   $scope.lastName= res.data.lastName;
-  //   $scope.userPic= res.data.pic;
-  // });
+  userStocksService.getUserInfo($stateParams.id).then(function(res){
+    // console.log(res);
+    $scope.firstName= res.data.firstName;
+    $scope.lastName= res.data.lastName;
+    $scope.userPic= res.data.pic;
+  });
 
   }, //closes controller
   bindings: []
