a95da439e7e0f3ef811c7f63ee3a73f5733562ce
Austin Walker <bgbrwr@gmail.com>
2016-11-01 09:42:29 -0600

    Working on D3 Graph

--- public/js/components/starredStocksComponent.js
+++ public/js/components/starredStocksComponent.js
@@ -1,24 +1,14 @@
 angular.module('stock').component('starredStocksComponent', {
-<<<<<<< HEAD
     templateUrl: "./js/templates/starredStocksComponent.html",
     controller: function starredStocksController(userStocksService, yahooService, $scope, $stateParams) {
 
         userStocksService.getSavedStocks($stateParams.id).then(function(res) {
             // console.log("starred", res.data);
-=======
-  templateUrl: "./js/templates/starredStocksComponent.html",
-  controller: function starredStocksController(userStocksService, yahooService, $scope, $stateParams){
-    console.log($stateParams);
-    userStocksService.getSavedStocks($stateParams.id)
-      .then(function(res){
-        console.log("starred", res.data);
->>>>>>> master
 
             //getting customers saved stocks for yahoo snaphsot
             var savedStockSymbols = {
                 symbols: []
             };
-<<<<<<< HEAD
             //passing saved stocks into a new array
             for (var i = 0; i < res.data.length; i++) {
                 savedStockSymbols.symbols.push(res.data[i].company_symbol);
@@ -30,21 +20,6 @@ angular.module('stock').component('starredStocksComponent', {
             }, function(err) {
                 console.log(err);
             });
-=======
-        //passing saved stocks into a new array
-        for (var i = 0; i < res.data.length; i++) {
-          savedStockSymbols.symbols.push(res.data[i].company_symbol);
-        }
-        console.log(savedStockSymbols);
-        //sending new array to backend for an api call
-        yahooService.getSnapshots(savedStockSymbols)
-        .then(function(res){
-          console.log(res.data);
-          $scope.saved_stocks = res.data;
-        }, function(err) {
-            console.log(err);
-        });
->>>>>>> master
 
             // console.log(res.data);
         }); //closes selectStocksService function
@@ -59,7 +34,7 @@ angular.module('stock').component('starredStocksComponent', {
     bindings: []
 
 })
-// 
+//
 // .controller('testCtrl', function($scope, $stateParams) {
 //
 //   console.log("testCtrl", $stateParams);
--- public/js/components/yahooComponent.js
+++ public/js/components/yahooComponent.js
@@ -1,66 +1,68 @@
-
 angular.module('stock').component('yahooComponent', {
-  templateUrl: "./js/templates/yahooComponent.html",
-  controller: function yahooController(yahooService, $stateParams, $scope){
-    yahooService.getStocks($stateParams.stockId).then(function(res){
-      $scope.stockData = res.data;
-      console.log(res.data);
+    templateUrl: "./js/templates/yahooComponent.html",
+    controller: function yahooController(yahooService, $stateParams, $scope) {
+        yahooService.getStocks($stateParams.stockId).then(function(res) {
+            $scope.stockData = res.data;
+            console.log(res.data);
 
-      var data13 = [];
-      $scope.stockData.map(function(data){
-        data13.push({"date": new Date(data.date), "close": data.close});
+            var data13 = [];
+            $scope.stockData.map(function(data) {
+                data13.push({
+                    "date": new Date(data.date),
+                    "close": data.close
+                });
 
-      });
-      var parseTime = d3.timeParse("%y-%b-%d");
+            });
+            var parseTime = d3.timeParse("%y-%b-%d");
 
+            var svg = d3.select("svg"),
+                margin = {
+                    top: 20,
+                    right: 20,
+                    bottom: 30,
+                    left: 50
+                },
+                width = +svg.attr("width") - margin.left - margin.right,
+                height = +svg.attr("height") - margin.top - margin.bottom,
+                g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+            var x = d3.scaleTime().rangeRound([0, width]);
 
-      var svg = d3.select("svg"),
-        margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            var y = d3.scaleLinear().rangeRound([height, 0]);
 
+            var line = d3.line().x(function(d) {
+                return x(d.date);
+            }).y(function(d) {
+                return y(d.close);
+            });
 
+            x.domain(d3.extent(data13, function(d) {
+                return d.date;
+            }));
+            y.domain(d3.extent(data13, function(d) {
+                return d.close;
+            }));
 
-      var x = d3.scaleTime()
-          .rangeRound([0, width]);
+            g.append("g").attr("class", "axis axis--x").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
 
-      var y = d3.scaleLinear()
-          .rangeRound([height, 0]);
+            g.append("g").attr("class", "axis axis--y").call(d3.axisLeft(y)).append("text").attr("fill", "#000").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", "0.71em").style("text-anchor", "end").text("Price ($)");
 
-      var line = d3.line()
-          .x(function(d) { return x(d.date); })
-          .y(function(d) { return y(d.close); });
-
-
-        x.domain(d3.extent(data13, function(d) { return d.date; }));
-        y.domain(d3.extent(data13, function(d) { return d.close; }));
-
-        g.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
-
-        g.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y))
-          .append("text")
-            .attr("fill", "#000")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "0.71em")
-            .style("text-anchor", "end")
-            .text("Price ($)");
-
-        g.append("path")
+            g.append("path")
             .datum(data13)
             .attr("class", "line")
-            .attr("d", line);
-});
+            .attr("d", line)
+            .on("mouseover", function(){return tooltip.style("visibility", "visible");})
+          	.on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px").text(d);})
+          	.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 
-  },
-  bindings: {
 
-  }
+            var tooltip = d3.select("body").append("div")
+            .style("position", "absolute")
+            .style("z-index", "10")
+            .style("visibility", "hidden");
+
+        });
+
+    },
+    bindings: {}
 });
