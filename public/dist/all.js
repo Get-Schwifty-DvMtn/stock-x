7c75f5e2c2cbdb1b5f6ec051956c55c5eab09594
dougglez <maxfield.douglas@gmail.com>
2016-11-01 10:56:16 -0600

    adding dist files so i can pull latest thing

--- public/dist/all.js
+++ public/dist/all.js
@@ -171,11 +171,7 @@ angular.module('stock').component('starredStocksComponent', {
     }, //closes controller
     bindings: []
 
-<<<<<<< HEAD
-})
-=======
 });
->>>>>>> master
 //
 // .controller('testCtrl', function($scope, $stateParams) {
 //
@@ -261,13 +257,21 @@ $scope.getNewsDay = function(){
 
 angular.module("stock")
   .service("nyTimesService", function($http){
-
-  this.getNews = function(companyData){
-  return $http.post("/stocknews", companyData);
-    // return $http({
-    //   method: "GET",
-    //   url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&begin_date=20120101&end_date=20120101&api-key=8c027c7e64c74f42a29665437c7db2a7",
-    // });
+  this.getNews = function(){
+    return $http({
+      method: "GET",
+      // url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+      url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&begin_date=20120101&end_date=20120101&api-key=8c027c7e64c74f42a29665437c7db2a7",
+      // qs: {
+      //   'api-key': config.nytAPI,
+      //   'q': "Apple",
+      //   'fq': "news_desk:(Business)",
+      //   'begin_date': "20161022",
+      //   'end_date': "20161024",
+      //   'sort': "newest",
+      //   'fl': "web_url,headline,snippet"
+      // },
+    });
   };
 });
 
