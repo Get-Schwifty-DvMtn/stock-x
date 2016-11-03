c94ed6c682598f1004948be6622fefec805cdc06
Austin Walker <Austin Walker>
2016-11-03 17:08:21 -0600

    Added persistent login. Still need logout button

--- server/controller/userCtrl.js
+++ server/controller/userCtrl.js
@@ -3,24 +3,13 @@ var db = app.get('db');
 
 module.exports = {
     getUserInfo: function(req, res) {
-        db.find_By_Id([req.params.id], function(err, user) {
+        db.find_By_Id([req.session.passport.user.google_id], function(err, user) {
             if (err) {
                 res.status(400).json(err);
-            } else if (user[0]) {
-                var userInfo = {
-                    firstName: user[0].first_name,
-                    lastName: user[0].last_name,
-                    pic: user[0].pic_url
-                }
-                res.status(200).json(userInfo);
-            } else if (user) {
-                var userInfo = {
-                    firstName: user.first_name,
-                    lastName: user.last_name,
-                    pic: user.pic_url
-                }
-                res.status(200).json(userInfo);
-            };
+            } else {
+
+                res.status(200).json(user[0]);
+            }
 
         });
     }
