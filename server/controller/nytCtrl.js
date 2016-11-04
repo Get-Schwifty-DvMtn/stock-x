5212763052b206fb898b2eb4aaba6e7ada433da6
Brad Frost <bfozzy@icloud.com>
2016-11-03 19:30:58 -0600

    transfering code over for a pull request

--- server/controller/nytCtrl.js
+++ server/controller/nytCtrl.js
@@ -4,12 +4,12 @@ var axios = require ("axios");
 module.exports = {
   getNews: function(req, response){
   var companyData = {
-    company: 'q='+req.body.company,
+    company: 'q='+req.body.companySearch,
     category: 'news_desk(\"market place\")',
     begin: "begin_date="+ req.body.begin,
     end: "end_date="+ req.body.end,
   };
-    axios.get( 'http://api.nytimes.com/svc/search/v2/articlesearch.json?'+ companyData.company+'&'+companyData.category+'&'+companyData.begin+'&'+companyData.end+'&api-key='+ config.nytAPI).then(function(res){
+    axios.get( 'http://api.nytimes.com/svc/search/v2/articlesearch.json?'+ companyData.companySearch+'&'+companyData.category+'&'+companyData.begin+'&'+companyData.end+'&api-key='+ config.nytAPI).then(function(res){
       response.status(200).json(res.data);
     });
   }
