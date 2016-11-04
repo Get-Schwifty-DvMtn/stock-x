2e52ad39686fc033e5e04661667b16bfea896363
Brad Frost <bfozzy@icloud.com>
2016-11-04 12:14:07 -0600

    now the change date functions are working on the graph

--- server/server.js
+++ server/server.js
@@ -107,7 +107,7 @@ app.get('/auth/google', passport.authenticate('google', {scope: ['https://www.go
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
 app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/#/'}), function(req, res) {
-    res.redirect('/#/user/' + req.user.google_id);
+    res.redirect('/#/user');
 });
 
 app.listen(config.port, function() {
