948484ea6469fdb1222b1c04dfe1420c52b6e994
Brad Frost <bfozzy@icloud.com>
2016-11-04 12:00:14 -0600

    need to merge

--- public/js/components/yahooComponent.js
+++ public/js/components/yahooComponent.js
@@ -8,37 +8,50 @@ angular.module("stock").component("yahooComponent", {
           begin: moment(start).format('YYYYMMDD'),
           end: moment(end).format('YYYYMMDD')
         };
+        $scope.newsDate = moment(start).format('MMMM Do, YYYY')
         nyTimesService.getNews(companyData).then(function(res){
           $scope.news = res.data.response.docs;
         });
-    };
-
+    }; // ends getNewsDay
+//
     function getDefaultNews(){
-    if (!$scope.newsDate){
-      var today = new Date();
-      $scope.newsDate = moment(new Date()).format('MMMM Do, YYYY');
-      $scope.getNewsDay(today, today, $scope.stockSearch);
-   }
-  }
-  
-  userStocksService.getOneStock($stateParams.stockId).then(function(res){
-     $scope.stockName = res.data[0].name;
-     $scope.stockSymbol = res.data[0].symbol;
-     $scope.stockSearch = res.data[0].search_term;
-     getDefaultNews();
-   });
+      if (!$scope.newsDate){
+        var today = new Date();
+        $scope.newsDate = moment(new Date()).format('MMMM Do, YYYY');
+        $scope.getNewsDay(today, today, $scope.stockSearch);
+      }
+    } // ends getDefaultNews
 
-    yahooService.getStocks($stateParams.stockId).then(function(res){
-      $scope.stockData = res.data;
-      $scope.stockSymbol = res.data[0].symbol;
+    userStocksService.getOneStock($stateParams.stockId).then(function(res){
+       $scope.stockName = res.data[0].name;
+       $scope.stockSymbol = res.data[0].symbol;
+       $scope.stockSearch = res.data[0].search_term;
+       getDefaultNews();
+       $scope.setGraphRange(12);
+     });
+//
+   var today = moment(new Date());
 
-      var data13 = [{"values": []}];
+$scope.setGraphRange = function(monthsStart){
+//
+     var stockData = {
+       stockSymbol: $scope.stockSymbol,
+       start: moment(new Date()).subtract(monthsStart, "months").format("YYYY-MM-DD"),
+       end: today.format("YYYY-MM-DD")
+     };
+     console.log("before http call",stockData);
+     yahooService.getStocks(stockData).then(function(res){
+       $scope.stockData = res.data;
+       $scope.stockSymbol = res.data[0].symbol;
+       console.log(res.data);
+
+         var data13 = [{"values": []}];
       $scope.stockData.map(function(data){
         data13[0].values.push({"date": new Date(data.date), "open": (data.open), "high": (data.high), "low": (data.low),  "close": (data.close), "volume": (data.volume), "adjusted": (data.adjClose)});
       });
-
+//
       var type = 'lineChart';
-
+//
       $scope.changeGraph = function (num) {
         if (num === 1) {
           type = 'lineChart';
@@ -53,7 +66,7 @@ angular.module("stock").component("yahooComponent", {
       };
       $scope.data = data13;
       $scope.options = {
-
+//
                 chart: {
                     showLegend: false,
                     type: type,
@@ -67,7 +80,7 @@ angular.module("stock").component("yahooComponent", {
                     x: function(d){ return d['date']; },
                     y: function(d){ return d['close']; },
                     duration: 100,
-
+//
                     xAxis: {
                         axisLabel: 'Dates',
                         tickFormat: function(d) {
@@ -102,14 +115,15 @@ angular.module("stock").component("yahooComponent", {
                         verticalOff: true,
                         unzoomEventType: 'dblclick.zoom'
                     }
-                }
-            };
+                } // this ends chart
+            }; // this ends scope.options
             $scope.config = {
               deepWatchOptions:true
-            };
+            }; // end scope.config
             setTimeout(function(){
                 $scope.api.refresh();
             },500);
-      });
-  }
-});
+      }); // ends yahooService.getStocks.then function
+    };//ends setGraphRange function
+  } // ends controller
+}); // Angular.module
