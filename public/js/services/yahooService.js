51cab22000063b7b883fdff5452c5a26633f0cbc
Brad Frost <bfozzy@icloud.com>
2016-11-04 09:44:04 -0600

    still working on graph timeframes

--- public/js/services/yahooService.js
+++ public/js/services/yahooService.js
@@ -2,7 +2,7 @@ angular.module("stock")
   .service("yahooService", function($http){
   this.getStocks = function(stockId){
     // console.log('yahooService', stockId);
-    return $http.get("/testhole/" + stockId);
+    return $http.get("/testhole/" + stockId + "/" + startDate + "/" + endDate); //changed
   };
   this.getSnapshots = function(symbols) {
     // console.log("service snapshots", symbols);
