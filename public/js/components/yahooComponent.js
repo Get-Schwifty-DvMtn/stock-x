e661d5e012dbb148fa72b93f2f63ab00caf3dab5
dougglez <maxfield.douglas@gmail.com>
2016-11-04 12:25:22 -0600

    beatufied yahoo component. tabs and spaces were changed

--- public/js/components/yahooComponent.js
+++ public/js/components/yahooComponent.js
@@ -1,71 +1,69 @@
 angular.module("stock").component("yahooComponent", {
-  templateUrl: "./js/templates/yahooComponent.html",
-  controller: function yahooController(yahooService,nyTimesService,userStocksService, $stateParams, $scope){
+    templateUrl: "./js/templates/yahooComponent.html",
+    controller: function yahooController(yahooService, nyTimesService, userStocksService, $stateParams, $scope) {
 
-    $scope.getNewsDay = function(start, end, company){
-        var companyData = {
-          company: company,
-          begin: moment(start).format('YYYYMMDD'),
-          end: moment(end).format('YYYYMMDD')
+        $scope.getNewsDay = function(start, end, company) {
+            var companyData = {
+                company: company,
+                begin: moment(start).format('YYYYMMDD'),
+                end: moment(end).format('YYYYMMDD')
+            };
+            nyTimesService.getNews(companyData).then(function(res) {
+                $scope.news = res.data.response.docs;
+            });
         };
-        nyTimesService.getNews(companyData).then(function(res){
-          $scope.news = res.data.response.docs;
+
+        function getDefaultNews() {
+            if (!$scope.newsDate) {
+                var today = new Date();
+                $scope.newsDate = moment(new Date()).format('MMMM Do, YYYY');
+                $scope.getNewsDay(today, today, $scope.stockSearch);
+            }
+        }
+
+        userStocksService.getOneStock($stateParams.stockId).then(function(res) {
+            $scope.stockName = res.data[0].name;
+            $scope.stockSymbol = res.data[0].symbol;
+            $scope.stockSearch = res.data[0].search_term;
+            getDefaultNews();
         });
-    };
 
-    function getDefaultNews(){
-    if (!$scope.newsDate){
-      var today = new Date();
-      $scope.newsDate = moment(new Date()).format('MMMM Do, YYYY');
-      $scope.getNewsDay(today, today, $scope.stockSearch);
-   }
-  }
-  
-  userStocksService.getOneStock($stateParams.stockId).then(function(res){
-     $scope.stockName = res.data[0].name;
-     $scope.stockSymbol = res.data[0].symbol;
-     $scope.stockSearch = res.data[0].search_term;
-     getDefaultNews();
-   });
+        yahooService.getStocks($stateParams.stockId).then(function(res) {
+            $scope.stockData = res.data;
+            $scope.stockSymbol = res.data[0].symbol;
 
-    yahooService.getStocks($stateParams.stockId).then(function(res){
-      $scope.stockData = res.data;
-      $scope.stockSymbol = res.data[0].symbol;
+            var data13 = [{ "values": [] }];
+            $scope.stockData.map(function(data) {
+                data13[0].values.push({ "date": new Date(data.date), "open": (data.open), "high": (data.high), "low": (data.low), "close": (data.close), "volume": (data.volume), "adjusted": (data.adjClose) });
+            });
 
-      var data13 = [{"values": []}];
-      $scope.stockData.map(function(data){
-        data13[0].values.push({"date": new Date(data.date), "open": (data.open), "high": (data.high), "low": (data.low),  "close": (data.close), "volume": (data.volume), "adjusted": (data.adjClose)});
-      });
+            var type = 'lineChart';
 
-      var type = 'lineChart';
-
-      $scope.changeGraph = function (num) {
-        if (num === 1) {
-          type = 'lineChart';
-        }
-        else if (num === 2) {
-          type = 'candlestickBarChart';
-        }
-        else if (num === 3) {
-          type = 'ohlcBarChart';
-        }
-        $scope.options.chart.type = type;
-      };
-      $scope.data = data13;
-      $scope.options = {
+            $scope.changeGraph = function(num) {
+                if (num === 1) {
+                    type = 'lineChart';
+                } else if (num === 2) {
+                    type = 'candlestickBarChart';
+                } else if (num === 3) {
+                    type = 'ohlcBarChart';
+                }
+                $scope.options.chart.type = type;
+            };
+            $scope.data = data13;
+            $scope.options = {
 
                 chart: {
                     showLegend: false,
                     type: type,
                     height: 450,
-                    margin : {
+                    margin: {
                         top: 20,
                         right: 20,
                         bottom: 40,
                         left: 60
                     },
-                    x: function(d){ return d['date']; },
-                    y: function(d){ return d['close']; },
+                    x: function(d) { return d['date']; },
+                    y: function(d) { return d['close']; },
                     duration: 100,
 
                     xAxis: {
@@ -78,18 +76,18 @@ angular.module("stock").component("yahooComponent", {
 
                     yAxis: {
                         axisLabel: 'Stock Price',
-                        tickFormat: function(d){
+                        tickFormat: function(d) {
                             return '$' + d3.format(',.1f')(d);
                         },
                         showMaxMin: false
                     },
                     lines: {
                         dispatch: {
-                          // THIS IS WHERE YOU CAN ACCESS THE POINT DATA
-                            elementClick: function(e){
-                            var searchDate = e.point.date;
-                            $scope.getNewsDay(searchDate, searchDate, $scope.stockName);
-                          }
+                            // THIS IS WHERE YOU CAN ACCESS THE POINT DATA
+                            elementClick: function(e) {
+                                var searchDate = e.point.date;
+                                $scope.getNewsDay(searchDate, searchDate, $scope.stockName);
+                            }
                         }
                     },
                     zoom: {
@@ -105,11 +103,11 @@ angular.module("stock").component("yahooComponent", {
                 }
             };
             $scope.config = {
-              deepWatchOptions:true
+                deepWatchOptions: true
             };
-            setTimeout(function(){
+            setTimeout(function() {
                 $scope.api.refresh();
-            },500);
-      });
-  }
+            }, 500);
+        });
+    }
 });
