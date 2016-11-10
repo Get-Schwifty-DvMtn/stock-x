63559616d04f14d756f37bc8861c3e65373be855
Brad Frost <bfozzy@icloud.com>
2016-11-10 09:45:21 -0700

    fixed search and removed a bunch of console logs

--- public/js/services/yahooService.js
+++ public/js/services/yahooService.js
@@ -1,11 +1,9 @@
 angular.module("stock")
   .service("yahooService", function($http){
   this.getStocks = function(stockData){
-    // console.log('yahooService', stockId);
     return $http.get("/getgraphdata/" + stockData.stockSymbol + "/" + stockData.start + "/" + stockData.end); //changed
   };
   this.getSnapshots = function(symbols) {
-    // console.log("service snapshots", symbols);
     return $http.post("/snapshots", symbols);
   };
 
